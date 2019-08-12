bb79cad47cc28ffc9ca687a64bddd9c5ee4f613b
thanhbn <thanhbn87@gmail.com>
2019-08-12 09:20:30 +0700

    change ttl to automatic

--- cloudflare/delegated_sub2route53/vars.tf
+++ cloudflare/delegated_sub2route53/vars.tf
@@ -6,4 +6,4 @@ variable "cf_token" {}
 variable "root_domain" { default = "example.com" }
 variable "domain_name" { default = "sub.example.com" }
 variable "domain_is_sub" { default = true }
-variable "cf_ttl" { default = "120" }
+variable "cf_ttl" { default = "1" }
